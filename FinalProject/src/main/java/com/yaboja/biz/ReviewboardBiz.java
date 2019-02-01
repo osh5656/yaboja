b9d7aa96d00f545556940fbd47665ae422a7312e
HyeongyuJung <hyeongyu_93@naver.com>
2019-02-01 09:25:48 +0900

    수호완료 진선이가작업할꺼

--- FinalProject/src/main/java/com/yaboja/biz/ReviewboardBiz.java
+++ FinalProject/src/main/java/com/yaboja/biz/ReviewboardBiz.java
@@ -1,10 +1,20 @@
 package com.yaboja.biz;
 
 import java.util.List;
+import java.util.Map;
 
+import com.yaboja.dto.Criteria;
+import com.yaboja.dto.MovieDto;
 import com.yaboja.dto.ReviewboardDto;
+import com.yaboja.dto.SearchCriteria;
 
 public interface ReviewboardBiz {
+
+	// 진선 추가
+	public List<MovieDto> movieList();
+
+	// 진선 추가
+	public MovieDto selectOne1(int movieseq);
 
 	public List<ReviewboardDto> selectList();
 
@@ -15,6 +25,21 @@ public interface ReviewboardBiz {
 	public int update(ReviewboardDto dto);
 
 	public int delete(int reviewboardseq);
-	
+
 	public int viewupdate(int reviewboardseq);
+
+	// 목록 + 페이징
+	public List<ReviewboardDto> listPage(Criteria cri);
+
+	// 게시물 총 개수
+	public int listCount();
+
+	// 진선쓰 페이징 토탈카운트
+	public int listCount1();
+
+	// 목록 + 페이징 + 검색
+	public List<MovieDto> listSearch(SearchCriteria scri);
+
+	// 검색 결과 개수
+	public int countSearch(SearchCriteria scri);
 }
